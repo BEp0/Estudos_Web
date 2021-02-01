364a4265aa4a350a3250e3dce8e022315a3b504c
Bernardo P S Souza <beportoss@gmail.com>
2021-02-01 16:44:01 -0300

    funcionando mela metado

---
+++ Aulas de JS/Aula12/exercicios/verificando idade/src.js
@@ -0,0 +1,21 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#resultado')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        alert('[ERRO] Verifique e tente novamente')
+    }else {
+        var fradgen = document.getElementsByName('radgen')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fradgen[0].checked) {
+            genero = 'Masculino'
+        } else if (fradgen[1].checked){
+            genero = 'Feminino'
+        } else {
+            genero = 'Não Binário'
+        }
+        res.innerHTML = `Detactamos ${genero} com ${idade} anos.`
+    }
+}
